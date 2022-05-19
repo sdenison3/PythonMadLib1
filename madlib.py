50ee1a91855082cef580b047f760820b0c9edda4
sdenison3 <101777006+sdenison3@users.noreply.github.com>
2022-05-18 19:58:34 -0500

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,22 @@
+noun1 = input("Noun(Animal) :")
+noun2 = input("Noun :")
+noun3 = input("Body part :")
+verb1 = input("Verb : ")
+adj1 = input("Adjective :")
+noun4 = input("Plural noun :")
+place = input("Place :")
+pastVerb1 = input("Past Tense Verb :")
+super1 = input("Superlative :")
+land = input("Geographical area(lake, desert, etc.) :")
+largeanimals = input("Large Animals :")
+pastVerb2 = input("Past Tense Verb :")
+verb2 = input("Verb :")
+noun5 = input("Noun :")
+
+madlib = f"There once was a {noun1} on a {noun2}. This was unlike any {noun1}. Its {noun3} majestically {verb1} in the wind.\
+    The only word that comes to mind when describing this {noun1} is {adj1}. The legend goes that this particular {noun1} \
+    overcame great {noun4}. It traveled from the land of {place}. It {pastVerb1} the {super1} {land}. Some have even said \
+    that this {noun1} ate {largeanimals} as a snack! However, it has one weakness. If it {pastVerb2} {noun5} there is no hope. It \
+    will surely {verb2}."
+    
+print(madlib)
